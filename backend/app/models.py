634f0a7e2ffc595269613f3c70e2514af2d13865
thulanesigasa <pharezsigasa@gmail.com>
2026-06-15 14:04:04 +0200

    feat: implement phase 4 part 2

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,6 +4,8 @@ from app.extensions import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    user_token = db.Column(db.String(120), nullable=False)
+    place_id = db.Column(db.String(120), nullable=False)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
 
